62e62739fcf19e116ba17e73c170a959b9333e2c
Bishan Wishvajith <167428653+wishvajithW3@users.noreply.github.com>
2026-09-08 10:19:12 +0530

    Fix class name and formatting in IT23267640Lab3Q4

--- IT23267640Lab3Q4.java
+++ IT23267640Lab3Q4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class IT26101660Lab3Q4 {
+public class IT23267640Lab3Q4 {
     public static void main(String[] args) {
         
         // Declare variables for the 5 digits
