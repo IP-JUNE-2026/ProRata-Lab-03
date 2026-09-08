1edf796cbb812b84b556286dd15bec5b96c67564
imalka lakshitha abekoon <it22175670@my.sliit.lk>
2026-09-08 11:34:58 +0530

    Add files via upload

---
+++ Q3/IT22175670Lab3Q3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class IT22175670Lab3Q3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the amount in rupees: ");
+        int amount = scanner.nextInt();
+
+        int[] denominations = {5000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int[] counts = new int[denominations.length];
+
+        for (int i = 0; i < denominations.length; i++) {
+            counts[i] = amount / denominations[i];
+            amount %= denominations[i];
+        }
+
+        System.out.println("Denominations:");
+        for (int i = 0; i < denominations.length; i++) {
+            System.out.println(denominations[i] + " Notes/Coins– " + counts[i]);
+        }
+
+        scanner.close();
+    }
+}
