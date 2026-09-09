db780dc613b1a5f899c19a143ad6d39e1677841e
It24102787 <it24102787@my.sliit.lk>
2026-09-09 22:59:51 +0530

    Add files via upload

---
+++ Q2/it24102787lab3q2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class it24102787lab3q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter the monthly salary: ");
+        double monthlySalary = scanner.nextDouble();
+        
+        System.out.print("Enter the number of OT hours: ");
+        double otHours = scanner.nextDouble();
+        
+        System.out.print("Enter the OT hourly rate: ");
+        double otHourlyRate = scanner.nextDouble();
+        
+        
+        double otAmount = otHours * otHourlyRate;
+        double totalSalary = monthlySalary + otAmount;
+        
+        
+        System.out.println("Total salary including OT: " + totalSalary);
+    }
+}
